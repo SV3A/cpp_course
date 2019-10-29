82faf54f3c21d9e9fecd9465e792a589b1b2e010
sv3a <sveas93@gmail.com>
2019-10-29 12:26:08 +0100

    Added exercise for w6

---
+++ d6/fracint.cc
@@ -0,0 +1,128 @@
+#include <iostream>
+// A class for fractions of integers
+// The goal of the exercise of this week is to implement a class of fractions of
+// integers supporting some basic operations like addition and multiplication.
+
+
+class fraction
+{
+public:
+  int numerator;
+  int denominator;
+
+  // Constructor with optional argument for n and d
+  fraction () {
+    numerator   = 1;
+    denominator = 1;
+  }
+
+  fraction (int n, int d) {
+    numerator   = n;
+    denominator = d;
+  }
+
+  // Methods to update the fraction: add, subtract, multiply, and division
+  void add(fraction f){
+    if (denominator == f.denominator) {
+      numerator += f.numerator;
+    } else {
+      numerator = numerator * f.denominator + f.numerator * denominator;
+      denominator = denominator * f.denominator;
+    }
+    simplify();
+  }
+
+  void sub(fraction f){
+    if (denominator == f.denominator) {
+      numerator -= f.numerator;
+    } else {
+      numerator = numerator * f.denominator - f.numerator * denominator;
+      denominator = denominator * f.denominator;
+    }
+    simplify();
+  }
+
+  void mult(fraction f) {
+    numerator *= f.numerator;
+    denominator *= f.denominator;
+    simplify();
+  }
+
+  void div(fraction f) {
+    numerator *= f.denominator;
+    denominator *= f.numerator;
+    simplify();
+  }
+
+  // Operator overloading associated with the methods above
+  fraction operator+(fraction const &f){
+    fraction res(numerator, denominator);
+    res.add(f);
+    return res;
+  }
+
+  fraction operator-(fraction const &f){
+    fraction res(numerator, denominator);
+    res.sub(f);
+    return res;
+  }
+
+  fraction operator*(fraction const &f){
+    fraction res(numerator, denominator);
+    res.mult(f);
+    return res;
+  }
+
+  fraction operator/(fraction const &f){
+    fraction res(numerator, denominator);
+    res.div(f);
+    return res;
+  }
+
+  // Display fraction on the "a/b" format
+  void display(void) {
+    std::cout << numerator << "/" << denominator << std::endl;
+  }
+
+  // Simplification method
+  void simplify(){
+    if (numerator == 0){
+      return;
+    } else if (numerator == denominator) {
+      // Return unity
+      numerator   = 1;
+      denominator = 1;
+    } else {
+      // Determine greatest common divisor using Euclid's algorithm
+      int remainder, gcd = 1;
+      int a = std::max(numerator, denominator);
+      int b = std::min(numerator, denominator);
+
+      remainder = a % b;
+      gcd = b; // Special case if remainder is 0 initially
+
+      while (remainder != 0) {
+        gcd = remainder;
+        remainder = b%remainder;
+        b = remainder;
+      }
+
+      numerator   = numerator/gcd;
+      denominator = denominator/gcd;
+    }
+  }
+};
+
+
+int main(void)
+{
+  fraction f1(1, 4);
+  fraction f2(1, 4);
+  fraction f3;
+
+  f3 = f2+f1;
+  f3.display();
+
+  f3.add(f1);
+  f3.display();
+}
